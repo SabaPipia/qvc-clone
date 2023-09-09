c946c296c3d1c583b569e28bd14f2801d114cb33
Saba <pipiasaba788@gmail.com>
2023-09-09 21:03:24 +0400

    fixed:search functionality, added: clear input function

--- components/CustomInput/index.tsx
+++ components/CustomInput/index.tsx
@@ -11,27 +11,45 @@ import Image from "next/image";
 
 import React, { useContext, useState } from "react";
 import { ProductItem } from "@/types";
+import Link from "next/link";
 
 function CustomInput() {
   const { searchItems, searchedData } = useContext(DataContext);
-  const [searchVisible, isSearchVisible] = useState(false);
+  const [searchVisible, setSearchVisible] = useState(false);
+  const [isInputClear, setIsInputClear] = useState(true);
+  const [searchValue, setSearchValue] = useState<string>("");
+
   const handleSearchInput = (e: any) => {
-    if (e.target.value.length >= 1) {
-      searchItems(e.target.value);
-      isSearchVisible(searchedData ? true : false);
+    e.length === 0 ? setIsInputClear(true) : setIsInputClear(false);
+    if (e.length >= 1) {
+      searchItems(searchValue);
+      setSearchVisible(searchedData ? true : false);
     } else {
-      isSearchVisible(false);
+      setSearchVisible(false);
     }
+  };
+  const handleClearClick = () => {
+    setSearchValue("");
+    setSearchVisible(false);
   };
   return (
     <div className="input-wrapper">
       <input
+        value={searchValue}
         type="text"
         className="input"
         placeholder="What can we help you discover?"
-        onChange={(e) => handleSearchInput(e)}
+        onChange={(e) => {
+          handleSearchInput(e.target.value);
+          setSearchValue(e.target.value);
+        }}
       />
-      <div className="clear-wrapper">
+      <div
+        className={`clear-wrapper ${
+          isInputClear ? "clear-wrapper-none" : null
+        }`}
+        onClick={handleClearClick}
+      >
         <Image
           src={deleteIcon}
           width={30}
@@ -47,47 +65,62 @@ function CustomInput() {
           <div className="search-drop-down__heading">
             <span>Found {searchedData.length} Results for '...'</span>
           </div>
-          {searchedData.map((item: ProductItem) => {
-            return (
-              <div className="searched-item-card" key={item.id}>
-                <div className="serached-item__image-wrapper">
-                  <Image
-                    src={item.thumbnail}
-                    width={60}
-                    height={60}
-                    alt="{data.thumbnail}"
-                  />
-                </div>
-                <div className="searched-item__info">
-                  <div className="searched-item__main-info">
-                    <span className="searched-item-info__title">
-                      {item.title}, {item.brand}|{" "}
-                      {item.description.split(" ").slice(0, 7).join(" ")} ...
-                    </span>
-                  </div>
-                  <div className="searched-item__main-price">
-                    {item.discountPercentage > 13 ? (
-                      <>
-                        <span className="searched-item-info__sale-price">
-                          $
-                          {(
-                            item.price -
-                            item.price * (item.discountPercentage / 100)
-                          ).toFixed(0)}
+          {searchedData.map((item: ProductItem, index: number) => {
+            if (index <= 2) {
+              return (
+                <div key={item.id}>
+                  <Link
+                    href={`/${item.category}/${item.id}`}
+                    className="searched-item-card"
+                    onClick={() => setSearchVisible(false)}
+                  >
+                    <div className="serached-item__image-wrapper">
+                      <Image
+                        src={item.thumbnail}
+                        width={60}
+                        height={60}
+                        alt="{data.thumbnail}"
+                      />
+                    </div>
+                    <div className="searched-item__info">
+                      <div className="searched-item__main-info">
+                        <span className="searched-item-info__title">
+                          {item.title}, {item.brand}|{" "}
+                          {item.description.split(" ").slice(0, 7).join(" ")}{" "}
+                          ...
                         </span>
-                        <span className="searched-item-info__price item-sale">
-                          ${item.price}
-                        </span>
-                      </>
-                    ) : (
-                      <span className="searched-item-info__price">
-                        ${item.price}
-                      </span>
-                    )}
-                  </div>
+                      </div>
+                      <div className="searched-item__main-price">
+                        {item.discountPercentage > 13 ? (
+                          <>
+                            <span className="searched-item-info__sale-price">
+                              $
+                              {(
+                                item.price -
+                                item.price * (item.discountPercentage / 100)
+                              ).toFixed(0)}
+                            </span>
+                            <span className="searched-item-info__price item-sale">
+                              ${item.price}
+                            </span>
+                          </>
+                        ) : (
+                          <span className="searched-item-info__price">
+                            ${item.price}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                  </Link>
+                  {index >= 2 ? (
+                    // fix link
+                    <div className="show-all-result">
+                      <Link href="#">Show All Results</Link>
+                    </div>
+                  ) : null}
                 </div>
-              </div>
-            );
+              );
+            }
           })}
         </div>
       ) : null}
