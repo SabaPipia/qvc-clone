1f043c652ef1caed6a5d217c7d18b2bdceb2ba94
Saba <pipiasaba788@gmail.com>
2023-09-14 19:02:12 +0400

    simple error handlig (more to do)

--- app/myaccount/create-account/page.tsx
+++ app/myaccount/create-account/page.tsx
@@ -19,39 +19,144 @@ export default function CreateAccount() {
     zip: "",
     phone: "",
   });
+  const [inputErrors, setInputErrors] = useState({
+    email: "",
+    password: "",
+    repassword: "",
+    title: "",
+    firstName: "",
+    lastName: "",
+    address: "",
+    floorAddress: "",
+    zip: "",
+    phone: "",
+  });
+
   const handleFloorClick = (e: any) => {
     e.preventDefault();
     setIsFloorAddressVisible(!isFloorAddressVisible);
   };
+  const handleInputBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,24}$/;
+    const firstNameRegex = /^.*$/;
+    const lastNameRegex = /^.*$/;
+    const addressRegex = /^.*$/;
+    const zipRegex = /^\d{5}(?:-\d{4})?$/;
+    const phoneRegex = /^\d{9}$/;
+    console.log(name);
+    switch (name) {
+      case "email":
+        if (!emailRegex.test(value)) {
+          setInputErrors({ ...inputErrors, email: "Invalid email address" });
+        } else {
+          setInputErrors({ ...inputErrors, email: "" });
+        }
+        break;
+      case "password":
+        if (!passwordRegex.test(value)) {
+          setInputErrors({ ...inputErrors, password: "Invalid password" });
+        } else {
+          setInputErrors({ ...inputErrors, password: "" });
+        }
+        break;
+      case "repassword":
+        if (inputValues.repassword != inputValues.password) {
+          setInputErrors({ ...inputErrors, repassword: "Invalid password" });
+        } else {
+          setInputErrors({ ...inputErrors, repassword: "" });
+        }
+        break;
+      case "title":
+        if (!inputValues.title) {
+          setInputErrors({ ...inputErrors, title: "Invalid title" });
+        } else {
+          setInputErrors({ ...inputErrors, title: "" });
+        }
+        break;
+      case "firstName":
+        if (!firstNameRegex.test(value)) {
+          setInputErrors({ ...inputErrors, firstName: "Invalid firstName" });
+        } else {
+          setInputErrors({ ...inputErrors, firstName: "" });
+        }
+        break;
+      case "lastName":
+        if (!lastNameRegex.test(value)) {
+          setInputErrors({ ...inputErrors, lastName: "Invalid lastName" });
+        } else {
+          setInputErrors({ ...inputErrors, lastName: "" });
+        }
+        break;
+      case "address":
+        if (!addressRegex.test(value)) {
+          setInputErrors({ ...inputErrors, address: "Invalid address" });
+        } else {
+          setInputErrors({ ...inputErrors, address: "" });
+        }
+        break;
+      case "floorAddress":
+        if (!addressRegex.test(value)) {
+          setInputErrors({
+            ...inputErrors,
+            floorAddress: "Invalid floorAddress",
+          });
+        } else {
+          setInputErrors({ ...inputErrors, floorAddress: "" });
+        }
+        break;
+      case "zip":
+        if (!zipRegex.test(value)) {
+          setInputErrors({ ...inputErrors, zip: "Invalid zip" });
+        } else {
+          setInputErrors({ ...inputErrors, zip: "" });
+        }
+        break;
+      case "phone":
+        if (!phoneRegex.test(value)) {
+          setInputErrors({ ...inputErrors, phone: "Invalid phone" });
+        } else {
+          setInputErrors({ ...inputErrors, phone: "" });
+        }
+        break;
+      default:
+        break;
+    }
+  };
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
+
     setInputValues({
       ...inputValues,
       [name]: value,
     });
   };
-  console.log(inputValues.email);
+  console.log(inputErrors);
   return (
     <div className="create-account-wrapper container">
       <form>
         <div className="email-input-wrapper input-wrapper">
           <input
             type="email"
-            className="email__input"
+            className={`email__input ${inputErrors.email ? "is-error" : ""}`}
             name="email"
             value={inputValues.email}
             onChange={handleInputChange}
+            onBlur={handleInputBlur}
           />
           <label
             className={`email__label ${inputValues.email ? "has-value" : ""}`}
           >
             Email Address
           </label>
+          <p>{inputErrors.email}</p>
         </div>
         <div className="password-input-wrapper input-wrapper">
           <input
             type="password"
-            className="password__input"
+            className={`password__input ${inputErrors.email ? "is-error" : ""}`}
             name="password"
             value={inputValues.password}
             onChange={handleInputChange}
@@ -60,6 +165,7 @@ export default function CreateAccount() {
             Create Password
           </label>
           <button>Show</button>
+          <p>{inputErrors.password}</p>
         </div>
         <div className="repassword-input-wrapper input-wrapper">
           <input
@@ -73,6 +179,7 @@ export default function CreateAccount() {
             Confirm Password
           </label>
           <button>Show</button>
+          <p>{inputErrors.repassword}</p>
         </div>
         <div className="billing-address-wrapper">
           <div className="user-information">
@@ -97,6 +204,7 @@ export default function CreateAccount() {
               <label className={`${inputValues.firstName ? "has-value" : ""}`}>
                 First Name
               </label>
+              <p>{inputErrors.firstName}</p>
             </div>
             <div className="user__last-name input-wrapper">
               <input
@@ -108,6 +216,7 @@ export default function CreateAccount() {
               <label className={`${inputValues.lastName ? "has-value" : ""}`}>
                 Last Name
               </label>
+              <p>{inputErrors.lastName}</p>
             </div>
           </div>
           <div className="billing__street-address input-wrapper">
