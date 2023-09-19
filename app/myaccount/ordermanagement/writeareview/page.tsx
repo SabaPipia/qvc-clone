30cada9a05187c7b5b2141cd3924548fcbdda792
Saba <pipiasaba788@gmail.com>
2023-09-19 13:42:40 +0400

    created: write a review empty page

---
+++ app/myaccount/ordermanagement/writeareview/page.tsx
@@ -0,0 +1,13 @@
+import "./page.scss";
+
+import React from "react";
+
+export default function WriteReview() {
+  return (
+    <div className="container">
+      <div className="write-review-page-wrapper">
+        <h1>Write a Review</h1>
+      </div>
+    </div>
+  );
+}
