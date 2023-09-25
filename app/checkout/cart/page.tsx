34e0bdf2c0c4cadebc612c6bb6373b036a8ae303
Saba <pipiasaba788@gmail.com>
2023-09-25 20:58:31 +0400

    empty cart ui

--- app/checkout/cart/page.tsx
+++ app/checkout/cart/page.tsx
@@ -37,92 +37,107 @@ export default function Cart() {
   return (
     <div className="container">
       <div className="component-wrapper cart-wrapper">
-        <Link href="/" className="continue-shopping">
-          Continue Shopping
-        </Link>
-        <div className="cart-item">
-          <div className="cart-item__product-wrapper">
-            {cartItem.map((item: any) => {
-              return (
-                <div key={item.id} className="main-cart-items-wrapper">
-                  <div className="product-information__content">
-                    <div className="content__image-wrapper">
-                      <Image
-                        src={item.cartI.images[0]}
-                        width={1000}
-                        height={1000}
-                        alt="product card"
-                      />
-                    </div>
-                    <div className="content__product-info">
-                      <span className="product-info__id">{item.id}</span>
-                      <h4 className="product-info__title">
-                        {item.cartI.title}, {item.cartI.brand}
-                      </h4>
-                      <span className="product-info__easy-pays">
-                        3 Easy Pays of $20.00 <Link href="#">(Details)</Link>
-                      </span>
-                      <div className="product-info__price-wrapper">
-                        {item.cartI.discountPercentage > 13 ? (
-                          <div>
-                            <span className="price__sale-price">
-                              $
+        {cartItem.length != 0 ? (
+          <>
+            <Link href="/" className="continue-shopping">
+              Continue Shopping
+            </Link>
+            <div className="cart-item">
+              <div className="cart-item__product-wrapper">
+                {cartItem.map((item: any) => {
+                  return (
+                    <div key={item.id} className="main-cart-items-wrapper">
+                      <div className="product-information__content">
+                        <div className="content__image-wrapper">
+                          <Image
+                            src={item.cartI.images[0]}
+                            width={1000}
+                            height={1000}
+                            alt="product card"
+                          />
+                        </div>
+                        <div className="content__product-info">
+                          <span className="product-info__id">{item.id}</span>
+                          <h4 className="product-info__title">
+                            {item.cartI.title}, {item.cartI.brand}
+                          </h4>
+                          <span className="product-info__easy-pays">
+                            3 Easy Pays of $20.00{" "}
+                            <Link href="#">(Details)</Link>
+                          </span>
+                          <div className="product-info__price-wrapper">
+                            {item.cartI.discountPercentage > 13 ? (
+                              <div>
+                                <span className="price__sale-price">
+                                  $
+                                  {(
+                                    item.cartI.price -
+                                    item.cartI.price *
+                                      (item.cartI.discountPercentage / 100)
+                                  ).toFixed(0)}
+                                </span>
+                                <span className="price__item-sale">
+                                  ${item.cartI.price}
+                                </span>
+                              </div>
+                            ) : (
+                              <span className="price__main-price">
+                                ${item.cartI.price}
+                              </span>
+                            )}
+                            <span className="price__tax">
+                              S&H $
                               {(
-                                item.cartI.price -
-                                item.cartI.price *
-                                  (item.cartI.discountPercentage / 100)
+                                (item.cartI.discountPercentage / 100) *
+                                item.cartI.price
                               ).toFixed(0)}
                             </span>
-                            <span className="price__item-sale">
-                              ${item.cartI.price}
-                            </span>
                           </div>
-                        ) : (
-                          <span className="price__main-price">
-                            ${item.cartI.price}
-                          </span>
-                        )}
-                        <span className="price__tax">
-                          S&H $
-                          {(
-                            (item.cartI.discountPercentage / 100) *
-                            item.cartI.price
-                          ).toFixed(0)}
-                        </span>
+                        </div>
+                      </div>
+                      <div className="product-information__action">
+                        <span>quantity: {item.quantity}</span>
+                        <div>
+                          <button onClick={() => handleRemoveButton(item)}>
+                            Remove
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="product-information__action">
-                    <span>quantity: {item.quantity}</span>
-                    <div>
-                      <button onClick={() => handleRemoveButton(item)}>
-                        Remove
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div className="cart-item__product-checkout">
-            <div className="checkout__subtotal">
-              <span>Subtotal({cartItem.length} item)</span>
-              <span>
-                {sum.toLocaleString("en-US", {
-                  style: "currency",
-                  currency: "USD",
+                  );
                 })}
-              </span>
+              </div>
+              <div className="cart-item__product-checkout">
+                <div className="checkout__subtotal">
+                  <span>Subtotal({cartItem.length} item)</span>
+                  <span>
+                    {sum.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}
+                  </span>
+                </div>
+                <div className="checkout__buttons">
+                  <button>Checkout</button>
+                  <button>
+                    <Image src={PayPal} alt="paypal icon" />
+                    Checkout
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="checkout__buttons">
-              <button>Checkout</button>
-              <button>
-                <Image src={PayPal} alt="paypal icon" />
-                Checkout
-              </button>
+          </>
+        ) : (
+          <div className="cart-empty">
+            <div>
+              <h3>Your cart is empty</h3>
+
+              <Link href="/" className="empty-link">
+                Start Shopping
+              </Link>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
